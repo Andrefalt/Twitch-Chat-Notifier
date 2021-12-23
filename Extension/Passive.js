05d9055cf3c9ff4a31049dada6c99dffa28968ed
Andrefalt <70585168+Andrefalt@users.noreply.github.com>
2021-12-23 17:51:22 -0300

    Created the structure for the extension

--- Extension/Passive.js
+++ Extension/Passive.js
@@ -2,3 +2,11 @@ var URLAudio;
 document.querySelector('#confirm').addEventListener('click', function herewegoagain(){
   URLAudio = document.querySelector('#uri').value;
 });
+
+document.querySelector('#language').addEventListener('change', function(){
+  if(this.value=="Português){
+     document.querySelector("h3").innerHTML="Se quiser ter outro audio como toque de notificação, cole o link dele aqui: ";
+  }else{
+     document.querySelector("h3").innerHTML="If you want to have a custom audio as a notification sound, just paste it's link here: ";
+  };
+});
